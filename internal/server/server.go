03cc3510560a80ee52e10f0250ac9db7f90b4ce3
Arkhip McNutt <ortiz47@mail.ru>
2024-10-09 15:59:10 +0300

    added remaining endpoints

--- internal/server/server.go
+++ internal/server/server.go
@@ -34,7 +34,7 @@ func (s server) setRoutes() {
 
 	s.mux.HandleFunc("GET /api/v1/cinema/{cinemaName}/repertoire/", func(w http.ResponseWriter, r *http.Request) {
 		cinemaName := r.PathValue("cinemaName")
-		res, err := s.serv.HandleRepertoire(cinemaName)
+		res, err := s.serv.GetCinemaRepertoire(cinemaName)
 		if err != nil {
 			w.Write([]byte(err.Error()))
 			w.WriteHeader(http.StatusInternalServerError)
@@ -51,7 +51,7 @@ func (s server) setRoutes() {
 
 	s.mux.HandleFunc("GET /api/v1/cinema/{cinemaName}/location/", func(w http.ResponseWriter, r *http.Request) {
 		cinemaName := r.PathValue("cinemaName")
-		res, err := s.serv.HandleLocation(cinemaName)
+		res, err := s.serv.GetCinemaLocation(cinemaName)
 		if err != nil {
 			res = err.Error()
 			if errors.Is(err, storage.ErrNotFound) {
@@ -65,7 +65,7 @@ func (s server) setRoutes() {
 
 	s.mux.HandleFunc("GET /api/v1/cinema/{cinemaName}/capacity/", func(w http.ResponseWriter, r *http.Request) {
 		cinemaName := r.PathValue("cinemaName")
-		res, err := s.serv.HandleMovieInfo(cinemaName)
+		res, err := s.serv.GetCinemaCapacity(cinemaName)
 		if err != nil {
 			res = err.Error()
 			if errors.Is(err, storage.ErrNotFound) {
@@ -80,7 +80,7 @@ func (s server) setRoutes() {
 
 	s.mux.HandleFunc("GET /api/v1/session/{sessionId}/remaining/", func(w http.ResponseWriter, r *http.Request) {
 		sessionId := r.PathValue("sessionId")
-		res, err := s.serv.HandleSessionRemaining(sessionId)
+		res, err := s.serv.GetSessionRemaining(sessionId)
 		if err != nil {
 			res = err.Error()
 			if errors.Is(err, storage.ErrNotFound) {
@@ -94,7 +94,7 @@ func (s server) setRoutes() {
 
 	s.mux.HandleFunc("GET /api/v1/session/{sessionId}/price/", func(w http.ResponseWriter, r *http.Request) {
 		sessionId := r.PathValue("sessionId")
-		res, err := s.serv.HandleSessionPrice(sessionId)
+		res, err := s.serv.GetSessionPrice(sessionId)
 		if err != nil {
 			res = err.Error()
 			if errors.Is(err, storage.ErrNotFound) {
@@ -108,7 +108,64 @@ func (s server) setRoutes() {
 
 	s.mux.HandleFunc("GET /api/v1/movie/{movieName}/info/", func(w http.ResponseWriter, r *http.Request) {
 		movieName := r.PathValue("movieName")
-		res, err := s.serv.HandleMovieInfo(movieName)
+		res, err := s.serv.GetMovieInfo(movieName)
+		if err != nil {
+			res = err.Error()
+			if errors.Is(err, storage.ErrNotFound) {
+				w.WriteHeader(http.StatusNotFound)
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		}
+
+		w.Write([]byte(res))
+	})
+
+	s.mux.HandleFunc("POST /api/v1/cinema/{cinemaName}/repertiore/{movieName}", func(w http.ResponseWriter, r *http.Request) {
+		cinemaName := r.PathValue("cinemaName")
+		movieName := r.PathValue("movieName")
+		res, err := s.serv.AddCinemaRepertoire(cinemaName, movieName)
+		if err != nil {
+			res = err.Error()
+			if errors.Is(err, storage.ErrNotFound) {
+				w.WriteHeader(http.StatusNotFound)
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		}
+
+		w.Write([]byte(res))
+	})
+
+	s.mux.HandleFunc("DELETE /api/v1/cinema/{cinemaName}/repertiore/{movieName}", func(w http.ResponseWriter, r *http.Request) {
+		cinemaName := r.PathValue("cinemaName")
+		movieName := r.PathValue("movieName")
+		res, err := s.serv.DeleteCinemaRepertoire(cinemaName, movieName)
+		if err != nil {
+			res = err.Error()
+			if errors.Is(err, storage.ErrNotFound) {
+				w.WriteHeader(http.StatusNotFound)
+			} else {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		}
+
+		w.Write([]byte(res))
+	})
+
+	s.mux.HandleFunc("POST /api/v1/cinema/{cinemaName}/{categoryName}", func(w http.ResponseWriter, r *http.Request) {
+		cinemaName := r.PathValue("cinemaName")
+		categoryName := r.PathValue("categoryName")
+		cinemaDistrict := r.FormValue("district")
+		cinemaStreet := r.FormValue("street")
+		cinemaBuiding := r.FormValue("building")
+		res, err := s.serv.AddCinema(
+			cinemaName,
+			categoryName,
+			cinemaDistrict,
+			cinemaStreet,
+			cinemaBuiding,
+		)
 		if err != nil {
 			res = err.Error()
 			if errors.Is(err, storage.ErrNotFound) {
--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -26,7 +26,7 @@ func New(user, password, dbname string) (*Storage, error) {
 	return storage, nil
 }
 
-func (s Storage) HandleRepertoire(cinemaName string) (res []string, err error) {
+func (s Storage) GetCinemaRepertoire(cinemaName string) (res []string, err error) {
 	query :=
 		`
 		SELECT
@@ -60,7 +60,7 @@ func (s Storage) HandleRepertoire(cinemaName string) (res []string, err error) {
 	return res, nil
 }
 
-func (s Storage) HandleLocation(cinemaName string) (res string, err error) {
+func (s Storage) GetCinemaLocation(cinemaName string) (res string, err error) {
 	query :=
 		`
 		SELECT 
@@ -90,7 +90,37 @@ func (s Storage) HandleLocation(cinemaName string) (res string, err error) {
 	return res, nil
 }
 
-func (s Storage) HandleSessionRemaining(sessionId string) (res string, err error) {
+func (s Storage) GetCinemaCapacity(cinemaName string) (res string, err error) {
+	query :=
+		`
+		SELECT
+		service_schema.cinema_category.capacity
+		FROM 
+		service_schema.cinema
+		JOIN 
+		service_schema.cinema_category
+		ON
+		service_schema.cinema.category_id = service_schema.cinema_category.id
+		WHERE 
+		service_schema.cinema.name = $1
+		`
+
+	row := s.db.QueryRow(query, cinemaName)
+
+	err = row.Scan(&res)
+	if err != nil {
+		log.Println("Error:", err)
+		if err == sql.ErrNoRows {
+			return res, ErrNotFound
+		} else {
+			return res, ErrInternal
+		}
+	}
+
+	return res, nil
+}
+
+func (s Storage) GetSessionRemaining(sessionId string) (res string, err error) {
 	query :=
 		`
 		SELECT
@@ -116,7 +146,7 @@ func (s Storage) HandleSessionRemaining(sessionId string) (res string, err error
 	return res, nil
 }
 
-func (s Storage) HandleSessionPrice(sessionId string) (res string, err error) {
+func (s Storage) GetSessionPrice(sessionId string) (res string, err error) {
 	query :=
 		`
 		SELECT
@@ -146,7 +176,7 @@ func (s Storage) HandleSessionPrice(sessionId string) (res string, err error) {
 	return res, nil
 }
 
-func (s Storage) HandleMovieInfo(movieName string) (res string, err error) {
+func (s Storage) GetMovieInfo(movieName string) (res string, err error) {
 	query :=
 		`
 		SELECT
@@ -172,5 +202,197 @@ func (s Storage) HandleMovieInfo(movieName string) (res string, err error) {
 	}
 
 	res = fmt.Sprintf("%v, %v", production, director)
+	return res, nil
+}
+
+func (s Storage) AddCinemaRepertoire(cinemaName, movieName string) (res string, err error) {
+
+	cinemaQuery :=
+		`
+		SELECT 
+		service_schema.cinema.id
+		FROM 
+		service_schema.cinema
+		WHERE 
+		service_schema.cinema.name = $1
+		`
+	cinemaRow := s.db.QueryRow(cinemaQuery, cinemaName)
+
+	cinemaId := ""
+	err = cinemaRow.Scan(&cinemaId)
+	if err != nil {
+		log.Println("Error:", err)
+		if err == sql.ErrNoRows {
+			return res, ErrNotFound
+		} else {
+			return res, ErrInternal
+		}
+	}
+
+	movieQuery :=
+		`
+		SELECT
+		service_schema.movie.id
+		FROM 
+		service_schema.movie
+		WHERE
+		service_schema.movie.name = $1
+		`
+	movieRow := s.db.QueryRow(movieQuery, movieName)
+
+	movieId := ""
+	err = movieRow.Scan(&movieId)
+	if err != nil {
+		log.Println("Error:", err)
+		if err == sql.ErrNoRows {
+			return res, ErrNotFound
+		} else {
+			return res, ErrInternal
+		}
+	}
+
+	query :=
+		`
+		INSERT INTO
+		service_schema.cinema_movies
+		VALUES
+		$1,
+		$2
+		`
+	row := s.db.QueryRow(query, cinemaId, movieId)
+
+	err = row.Scan(&res)
+	if err != nil {
+		log.Println("Error:", err)
+		if err == sql.ErrNoRows {
+			return res, ErrNotFound
+		} else {
+			return res, ErrInternal
+		}
+	}
+
+	return res, nil
+}
+
+func (s Storage) DeleteCinemaRepertoire(cinemaName, movieName string) (res string, err error) {
+	cinemaQuery :=
+		`
+	SELECT 
+	service_schema.cinema.id
+	FROM 
+	service_schema.cinema
+	WHERE 
+	service_schema.cinema.name = $1
+	`
+	cinemaRow := s.db.QueryRow(cinemaQuery, cinemaName)
+
+	cinemaId := ""
+	err = cinemaRow.Scan(&cinemaId)
+	if err != nil {
+		log.Println("Error:", err)
+		if err == sql.ErrNoRows {
+			return res, ErrNotFound
+		} else {
+			return res, ErrInternal
+		}
+	}
+
+	movieQuery :=
+		`
+		SELECT
+		service_schema.movie.id
+		FROM 
+		service_schema.movie
+		WHERE
+		service_schema.movie.name = $1
+		`
+	movieRow := s.db.QueryRow(movieQuery, movieName)
+
+	movieId := ""
+	err = movieRow.Scan(&movieId)
+	if err != nil {
+		log.Println("Error:", err)
+		if err == sql.ErrNoRows {
+			return res, ErrNotFound
+		} else {
+			return res, ErrInternal
+		}
+	}
+
+	query :=
+		`
+		DELETE FROM 
+		service_schema.cinema_movies
+		WHERE
+		service_schema.cinema_movies.cinema_id = $1
+		AND
+		service_schmea.cinema_movies.movie_id = $2
+		`
+	row := s.db.QueryRow(query, cinemaId, movieId)
+
+	err = row.Scan(&res)
+	if err != nil {
+		log.Println("Error:", err)
+		if err == sql.ErrNoRows {
+			return res, ErrNotFound
+		} else {
+			return res, ErrInternal
+		}
+	}
+
+	return res, nil
+}
+
+func (s Storage) AddCinema(cinemaName, categoryName, district, street, building string) (res string, err error) {
+	categoryQuery :=
+		`
+	SELECT 
+	service_schema.cinema_category.id
+	FROM 
+	service_schema.cinema_category
+	WHERE 
+	service_schema.cinema_category.name = $1
+	`
+	cinemaRow := s.db.QueryRow(categoryQuery, categoryName)
+
+	categoryId := ""
+	err = cinemaRow.Scan(&categoryId)
+	if err != nil {
+		log.Println("Error:", err)
+		if err == sql.ErrNoRows {
+			return res, ErrNotFound
+		} else {
+			return res, ErrInternal
+		}
+	}
+
+	query :=
+		`
+		INSERT INTO
+		service_schema.cinema
+		category_id,
+		name,
+		district,
+		street,
+		building
+		VALUES
+		$1,
+		$2,
+		$3,
+		$4,
+		$5
+		`
+	row := s.db.QueryRow(query, categoryId, cinemaName, district, street, building)
+
+	err = row.Scan(&res)
+	if err != nil {
+		log.Println("Error:", err)
+		if err == sql.ErrNoRows {
+			return res, ErrNotFound
+		} else {
+			return res, ErrInternal
+		}
+	}
+
 	return res, nil
 }
